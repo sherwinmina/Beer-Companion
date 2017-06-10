14d66efafa6a399e5e8496f7b19b2761140c4300
Sherwin Mina <sherwinmina@yahoo.com>
2017-06-10 10:26:20 -0700

    Added few more links to navigation

--- src/client/components/screens/hops.jsx
+++ src/client/components/screens/hops.jsx
@@ -2,87 +2,52 @@ import React, { Component } from 'react'
 import {Link} from 'react-router';
 import {Collapsible, CollapsibleItem} from 'react-materialize';
 import axios from 'axios';
+import _ from 'lodash';
+
 
 class Hops extends Component {
   constructor(props) {
     super(props);
     this.state = { myhops: [] };
   }
- 
-  componentDidMount() {
+
+  requestData() {
     const request = axios.get('../../../../../data/hops.json')
     .then(response => {
-      
       response.data.map((hop) => {
-              console.log(hop.name) 
-      });
-      
-      this.setState({myhops: response.data});
+        // console.log(hop.name, hop.notes) 
+        this.setState({myhops: hop.name})
+      });  
     })
+  }
+ 
+  componentDidMount() {
+    this.requestData();
   }
   
   renderList() {
-
     return this.state.myhops.map((hop) => {
-      return (        
-          <CollapsibleItem key={hop.id} header={hop.name} accordion='true'
-          > 
-            
-          </CollapsibleItem>  
-        );
+      return (
+        <li
+          key={hop.name}
+          className="list-group-item">
+          {hop.name}
+        </li>
+      );
     });
   }
 
   render () {
+    if (!this.state.myhops){
+      return <div>Loading..</div>
+    }
     return (
       <div>
-        <h4 className="center-align">Varity of hopsHops</h4>
-        <div className="collapsible">{this.renderList()}</div> 
-        {console.log(this.state.myhops)}
-        {this.state.myhops}
-       
+        <h4 className="center-align">Variety of Hops</h4>
+        {this.renderList()}  
       </div>
     )
   }
 }
-
-const hops = [
-    { name: "Simcoe",
-      description: "In 2000, Select Botanicals Group released the Simcoe Brand YCR 14 hop variety which is an aroma variety known for its versatility and unique characteristics. Simcoe Brand hops boasts several different aromas including passion fruit, pine, earthy, and citrus, and also has strong bittering qualities with an alpha acid level of 12%-14%. It is used extensively in the craft and home brewing industry."
-    }, 
-    { name: "Chinook",
-      description: "Popular American development in 1976 of the English Fuggle. Named for the Willamette Valley, an important hop-growing area. It has a character similar to Fuggle, but is more fruity and has some floral notes."
-    }, 
-    { name: "Amarillo",
-      description: "A high alpha acid bittering hop with mild flavor and low aromatic characteristics. Commercial examples include. 12-15% alpha acid."
-    }
-  ];
-
-  // const hops = axios.get('../../../../../data/hops.json')
-  //   .then(response => {
-  //     console.log((response));
-  //     return response.data;
-      
-  //   })
-
-// function fetchHops() {
-//   // const ROOT_URL = `'../../../../../data/hops.json'`;
-
-//   const url = '../../../../../data/hops.json';
-//   const request = axios.get(url)
-//     .then(response => {
-//       // console.log('data: ', {response})
-//       return {data: response.data}
-//     });
-
-//   // console.log('Request:', request);
-
-//   return request
- 
-// }
-
-// let hops = fetchHops();
-// console.log(hops);
-
-   
+  
 export default Hops;
